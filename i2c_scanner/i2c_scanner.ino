883ff954f7774e7107e505339e901480b0099b9e
drjquang <thelocaltourguide.saigon@gmail.com>
2019-11-12 16:59:06 +0700

    191110_I2C scanner, used for I2C devices, such as LCD driver(0x3F), RTC DS3231, EEPROM 24C32, I/O extender PCF8574 ...

---
+++ i2c_scanner/i2c_scanner.ino
@@ -0,0 +1,52 @@
+/*
+ * Date: Tue, 12/11/2019
+ * Desc: UNO, I2C address scanner
+ * Info: A4=SDA, A5=SCL
+ */
+
+#include <Wire.h>
+void setup() {
+  // Initialize Wire and Serial
+  Wire.begin(); 
+  Serial.begin(9600);
+  Serial.println("I2C Scanner is scanning: ");
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+ 
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+ 
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+ 
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+ 
+  delay(5000);           // wait 5 seconds for next scan
+}
